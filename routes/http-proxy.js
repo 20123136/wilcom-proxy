044d32219272ab5840fdd396ad75637f320507a4
zhangna <zhangna@wilcom.com.cn>
2019-04-17 12:26:56 +0800

    a

---
+++ routes/http-proxy.js
@@ -0,0 +1,65 @@
+const httpProxy = require('http-proxy');
+let proxy = httpProxy.createProxyServer();
+
+var stripJsonComments  = require('strip-json-comments');//扒光json中的注解
+var fs = require('fs'); // file system
+var config = JSON.parse(stripJsonComments(fs.readFileSync('config/default.json').toString()));
+
+
+proxy.on('proxyReq', function (proxyReq, req, res) {
+    if (req.body) {
+        let reqBody = JSON.stringify(req.body);
+        console.log("--------- req from client agent --------------", reqBody);
+        proxyReq.setHeader('Content-Length', Buffer.byteLength(reqBody, 'utf8'));
+        proxyReq.setHeader('Authorization', config.apollo.token);
+        proxyReq.setHeader('Content-Type', 'application/json;charset=UTF-8');
+        proxyReq.write(reqBody);
+        proxyReq.end();
+    }
+});
+proxy.on('proxyRes', function (proxyRes, req, res) {
+    // 设置允许跨域访问该服务.
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+    res.header("Access-Control-Max-Age", "600"); // 将预检请求的结果(即 Access-Control-Allow-Methods 和Access-Control-Allow-Headers 提供的信息)缓存10分钟
+    res.header("Access-Control-Allow-Headers", "content-type, Accept, X-Requested-With, remember-me");
+
+    var resBody = new Buffer('');
+    proxyRes.on('data', function (data) {
+        resBody = Buffer.concat([resBody, data]);
+    });
+    proxyRes.on('end', function () {
+        resBody = resBody.toString();
+        console.log("--------- res from proxied server -------------", resBody);
+    });
+});
+proxy.on('error', function (err, req, res) {
+    res.writeHead(500, {
+        'Content-Type': 'text/plain'
+    });
+    res.end('Something went wrong. And we are reporting a custom error message.');
+});
+
+
+/**
+ * req 的一些参数示例：
+ * url: '/loginPortal',
+ * baseUrl: '/passive-recorder',
+ * originalUrl: '/passive-recorder/loginPortal',
+ */
+module.exports = function (req, res, hostinfo, url) {
+    url = url == undefined ? "" : url;
+    console.log('===================================http://' + hostinfo.host + ':' +  hostinfo.port + url);
+    proxy.web(req, res, {
+        target: 'http://' + hostinfo.host + ':' +  hostinfo.port + url,
+    },(e)=>{
+        console.log("proxy error call back ");
+        console.log(e);
+        res.json(e);
+    });
+};
+
+
+
+
---
+++ routes/wilcom-routes.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+
+let wilcomproxy = require('http-proxy');
+var stripJsonComments  = require('strip-json-comments');//扒光json中的注解
+var fs = require('fs'); // file system
+var config = JSON.parse(stripJsonComments(fs.readFileSync('config/default.json').toString()));
+
+
+
+
+
+
+/**
+ *************************** 配置相应的路径 start *****************************
+ * 示例：
+ * 前端地址书写：/apollo/openapi/v1/apps
+ * 后端实际地址：/openapi/v1/apps
+ * 解释：前端相对于后端的地址，多出来的 apollo 字符串是为了匹配路由，该字符串不会被代理到后端 url 中。
+ *      如果要将这类匹配路由的字符串也要拼接到后端 url 中，请在 routes(req, res, url) 方法中设置第三个参数为该字符串或根据实际情况设定该字符串值。
+ *
+ */
+
+router.use('/security', function (req, res) {
+  routes(req, res, config.security, req.baseUrl);
+})
+
+router.use('/apollo', function (req, res) {
+  routes(req, res, config.apollo);
+})
+
+
+/**
+ *************************** 配置相应的路径 end *****************************
+ */
+
+
+
+
+
+
+
+/**
+ * 此方法为前端提供配置文件 default.json 中某个服务的所有配置信息。
+ */
+router.use('/getDefaultJson/:key', function (req, res) {
+  res.json(config[req.params.key]);
+})
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+
+module.exports = router;
